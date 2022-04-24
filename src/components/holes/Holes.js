ee1c6c8bf00374ac27d57d63ea628e5960485400
Anton Bies <antonbies@gmail.com>
2022-04-24 13:49:38 +0200

    changes Holes to array

--- src/components/holes/Holes.js
+++ src/components/holes/Holes.js
@@ -1,41 +1,15 @@
-export const holes = {
-  "01": {
-    par: 3
-  },
-  "02": {
-    par: 3
-  },
-  "03": {
-    par: 4
-  },
-  "04": {
-    par: 3
-  },
-  "05": {
-    par: 4
-  },
-  "06": {
-    par: 5
-  },
-  "07": {
-    par: 3
-  },
-  "08": {
-    par: 3
-  },
-  "09": {
-    par: 3
-  },
-  "10": {
-    par: 3
-  },
-  "11": {
-    par: 2
-  },
-  "12": {
-    par: 3
-  },
-  "13": {
-    par: 3
-  }
-}
+export const holes = [
+  {hole: "01", par: 3},
+  {hole: "02", par: 3},
+  {hole: "03", par: 4},
+  {hole: "04", par: 3},
+  {hole: "05", par: 4},
+  {hole: "06", par: 5},
+  {hole: "07", par: 3},
+  {hole: "08", par: 3},
+  {hole: "09", par: 3},
+  {hole: "10", par: 3},
+  {hole: "11", par: 2},
+  {hole: "12", par: 3},
+  {hole: "13", par: 3}
+];
